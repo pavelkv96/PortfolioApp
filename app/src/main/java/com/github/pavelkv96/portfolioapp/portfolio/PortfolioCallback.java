53a7a1f27fada161d277143bdaa88afe03775033
Pavel Kalinin <pavelkv96@gmail.com>
2021-03-07 10:55:34 +0300

    Added start project

---
+++ app/src/main/java/com/github/pavelkv96/portfolioapp/portfolio/PortfolioCallback.java
@@ -0,0 +1,7 @@
+package com.github.pavelkv96.portfolioapp.portfolio;
+
+public interface PortfolioCallback {
+
+    void onPortfolioItemClick(int pos);
+
+}
---
+++ app/src/main/java/com/github/pavelkv96/portfolioapp/sidemenu/Callback.java
@@ -0,0 +1,7 @@
+package com.github.pavelkv96.portfolioapp.sidemenu;
+
+public interface Callback {
+
+    void onSideMenuItemClick(int i);
+
+}
